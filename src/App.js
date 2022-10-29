c9ac56f2ba0cbacb800124084e14c97c67ce8562
Mohammed <shaki2632@gmail.com>
2022-10-29 22:39:57 +0000

    finish

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
         <section>
           <div>
             <h3 className='text-3xl py-1 dark:text-gray-400'>Portfolio</h3>
-            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-500'>Some of the projects I've worked on during college:</p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-500'>Some of the projects I've worked on during my journey at university:</p>
           </div>
       
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
